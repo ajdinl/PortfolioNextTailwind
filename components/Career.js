63ff78d4a1c05a05fcaf75230b26884b080266cb
ajdinl <ajdinl@live.com>
2021-11-02 20:34:21 +0100

    json data updated and added to application

--- components/Career.js
+++ components/Career.js
@@ -1,4 +1,9 @@
+import user from '../data/user.json'
+
 const Career = () => {
+	const { employer1, employer2 } = user.career
+	const { college, highSchool } = user.education
+
 	return (
 		<div className='flex flex-col pt-2'>
 			<div className='bg-white p-6 w-64 h-auto rounded-xl sm:mt-24 mt-0'>
@@ -8,45 +13,35 @@ const Career = () => {
 				<div className='text-mygrey text-md bg-transparent'>
 					<div className='text-mygrey text-sm bg-transparent'>
 						<div className='bg-transparent font-bold text-black mb-1'>
-							Service Technician
+							{employer1.headline}
 						</div>
 						<div className='mb-2'>
-							<div className='bg-white text-black'>Xenon-forte d.o.o.</div>
-							<div className='bg-white text-black'>Jan 2021 to Nov 2021</div>
+							<div className='bg-white text-black'>{employer1.employer}</div>
+							<div className='bg-white text-black'>{employer1.period}</div>
 						</div>
 						<div className='ml-4 bg-white'>
 							<ul className='list-disc mb-4'>
-								<li className='bg-white'>
-									Troubleshooting hardware and software
-								</li>
-								<li className='bg-white'>
-									Printer and computer diagnostic and service
-								</li>
-								<li className='bg-white'>
-									Installation and configuration of print / scan equipment,
-									computers and more
-								</li>
+								{employer1.obligations.map((obligation, index) => (
+									<li className='bg-white' key={index}>
+										{obligation}
+									</li>
+								))}
 							</ul>
 						</div>
 						<div className='bg-transparent font-bold text-black mb-1 pb-2'>
-							Administrative and technical assistant
+							{employer2.headline}
 						</div>
 						<div className='mb-2'>
-							<div className='bg-white text-black'>Accounting d.o.o.</div>
-							<div className='bg-white text-black'>Jan 2015 to Dec 2020</div>
+							<div className='bg-white text-black'>{employer2.employer}</div>
+							<div className='bg-white text-black'>{employer2.period}</div>
 						</div>
 						<div className='ml-4 bg-white'>
 							<ul className='list-disc mb-4'>
-								<li className='bg-white'>
-									Coordinating and performing administrative tasks
-								</li>
-								<li className='bg-white'>
-									Provide operational support to the team
-								</li>
-								<li className='bg-white'>
-									Creating and posting documentation, keeping a budget and cash
-									register table
-								</li>
+								{employer2.obligations.map((obligation, index) => (
+									<li className='bg-white' key={index}>
+										{obligation}
+									</li>
+								))}
 							</ul>
 						</div>
 					</div>
@@ -58,19 +53,22 @@ const Career = () => {
 				</div>
 				<div className='text-mygrey text-sm bg-transparent'>
 					<div className='bg-transparent font-bold text-black'>
-						Faculty of Political Science, University of Sarajevo
+						{college.headline}
 					</div>
-					<div className='bg-transparent mb-1 mt-1 text-black'>2012 - 2014</div>
+					<div className='bg-transparent mb-1 mt-1 text-black'>
+						{college.period}
+					</div>
 					<div className='bg-transparent mb-3 text-black'>
-						Bachelor of Business Communication.
+						{college.profession}
 					</div>
 					<div className='bg-transparent font-bold text-black'>
-						J.U. „Željeznički school centar“ Sarajevo and J.U. „Mješovita
-						srednja škola“ Gračanica
+						{highSchool.headline}
 					</div>
-					<div className='bg-transparent mb-1 mt-1 text-black'>2003 - 2007</div>
+					<div className='bg-transparent mb-1 mt-1 text-black'>
+						{highSchool.period}
+					</div>
 					<div className='bg-transparent mb-3 pb-2 text-black'>
-						Electrical technician.
+						{highSchool.profession}
 					</div>
 				</div>
 			</div>
--- components/Personal.js
+++ components/Personal.js
@@ -1,15 +1,16 @@
+import user from '../data/user.json'
+
 const Personal = () => {
+	const { profile, languages, more, other, personalities, hobbies } =
+		user.personal
+
 	return (
 		<div className='flex flex-col pt-2'>
 			<div className='bg-white p-6 w-64 h-60 sm:mt-24 mt-0 rounded-xl'>
 				<div className='text-mygreen font-bold bg-transparent mb-4'>
 					Personal Profile
 				</div>
-				<div className='text-mygrey text-md bg-transparent'>
-					I am a web developer with robust problem-solving skills. Self-taught
-					in React and Node js, with a desire for constant learning and problem-
-					solving.
-				</div>
+				<div className='text-mygrey text-md bg-transparent'>{profile}</div>
 			</div>
 			<div className='bg-white p-6 w-64 h-auto mt-8 rounded-xl'>
 				<div className='text-mygreen font-bold bg-transparent mb-4'>Skills</div>
@@ -17,16 +18,11 @@ const Personal = () => {
 					<div className='bg-transparent font-bold text-black'>
 						Coding Languages:
 					</div>
-					<div className='bg-transparent mb-3 mt-1'>JavaScript, HTML/CSS</div>
+					<div className='bg-transparent mb-3 mt-1'>{languages}</div>
 					<div className='bg-transparent font-bold text-black'>More JS:</div>
-					<div className='bg-transparent mb-3 mt-1'>
-						React, React Router, Redux/Recoil, Node, Express, Mongoose(MongoDB)
-					</div>
+					<div className='bg-transparent mb-3 mt-1'>{more}</div>
 					<div className='bg-transparent font-bold text-black'>Other:</div>
-					<div className='bg-transparent mb-3 mt-1'>
-						Bootstrap, Tailwind CSS, Adobe Photoshop, Lightroom, Linux, Visual
-						Studio
-					</div>
+					<div className='bg-transparent mb-3 mt-1'>{other}</div>
 				</div>
 			</div>
 			<div className='bg-white p-6 w-64 h-80 mt-8 rounded-xl mb-10'>
@@ -40,10 +36,11 @@ const Personal = () => {
 						</div>
 						<div className='ml-4 bg-white'>
 							<ul className='list-disc mb-4'>
-								<li className='bg-white'>Accuracy</li>
-								<li className='bg-white'>Creativity</li>
-								<li className='bg-white'>Communication</li>
-								<li className='bg-white'>Organization</li>
+								{personalities.map((personality, index) => (
+									<li className='bg-white' key={index}>
+										{personality}
+									</li>
+								))}
 							</ul>
 						</div>
 						<div className='bg-transparent font-bold text-black mb-1'>
@@ -51,10 +48,11 @@ const Personal = () => {
 						</div>
 						<div className='ml-4 bg-white'>
 							<ul className='list-disc mb-4'>
-								<li className='bg-white'>CryptoCurrency</li>
-								<li className='bg-white'>Motorcycling</li>
-								<li className='bg-white'>Skiing</li>
-								<li className='bg-white'>Photography</li>
+								{hobbies.map((hobby, index) => (
+									<li className='bg-white' key={index}>
+										{hobby}
+									</li>
+								))}
 							</ul>
 						</div>
 					</div>
