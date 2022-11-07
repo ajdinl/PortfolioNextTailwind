843795be0ffe47e51c77c53eef32818b953d8cdb
ajdinl <ajdinl@live.com>
2022-11-07 17:51:04 +0100

    remove App component

--- app/page.js
+++ app/page.js
@@ -1,5 +1,24 @@
-import App from './App'
+import Button from '@components/Button'
+import Info from '@components/Info'
+import Personal from '@components/Personal'
+import Career from '@components/Career'
 
-export default function Page() {
-  return <App />
+async function getData() {
+  const res = await fetch(process.env.FETCH_URL)
+  return res.json()
+}
+
+export default async function Page() {
+  const data = await getData()
+
+  return (
+    <>
+      <Button />
+      <div className='flex flex-row flex-wrap dark:bg-[#252525] pt-36 space-y-6 space-x-0 sm:space-x-6 justify-center min-h-screen bg-mybackground '>
+        <Info infoData={data.info} />
+        <Personal personalData={data.personal} />
+        <Career careerData={data.career} />
+      </div>
+    </>
+  )
 }
