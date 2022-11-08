d92be736e0b4943b39beb83cf5ae2d03de25f964
ajdinl <ajdinl@live.com>
2022-11-08 10:01:40 +0100

    remove pages folder

--- app/HomePage.js
+++ app/HomePage.js
@@ -11,7 +11,6 @@ export default function HomePage({ data }) {
   return (
     <>
       <Button />
-
       <div className='flex flex-row flex-wrap dark:bg-[#252525] pt-36 space-y-6 space-x-0 sm:space-x-6 justify-center min-h-screen bg-mybackground '>
         <Info infoData={info} />
         <Personal personalData={personal} />
--- app/page.js
+++ app/page.js
@@ -1,16 +1,12 @@
-// import HomePage from './HomePage'
+import HomePage from './HomePage'
 
-// async function getData() {
-//   const res = await fetch(process.env.FETCH_URL)
-//   return res.json()
-// }
+async function getData() {
+  const res = await fetch(process.env.FETCH_URL)
+  return res.json()
+}
 
 export default async function Page() {
-  // const data = await getData()
+  const data = await getData()
 
-  return (
-    <div>
-      <h1>Test page</h1>
-    </div>
-  )
+  return <HomePage data={data} />
 }
