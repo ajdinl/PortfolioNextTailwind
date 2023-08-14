02ebcbc8f924896f0dd036a6040a7022f2f64573
ajdinl <ajdinl@live.com>
2023-08-14 13:11:22 +0200

    lets try this

--- components/HomePage.js
+++ components/HomePage.js
@@ -17,6 +17,11 @@ export default function HomePage({ data }) {
         <Personal personalData={personal} />
         <Career careerData={career} />
       </div>
+      <div className='flex flex-row flex-wrap dark:bg-[#252525] pt-36 space-y-6 space-x-0 sm:space-x-6 justify-center min-h-screen bg-mybackground '>
+        <Info infoData={info} />
+        <Personal personalData={personal} />
+        <Career careerData={career} />
+      </div>
     </ThemeProvider>
   )
 }
