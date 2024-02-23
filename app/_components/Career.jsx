3b644632562030290fa42428d58c05d326e61f38
ajdinl <ajdinl@live.com>
2024-02-23 12:06:54 +0100

    upgrade hover effect

--- app/_components/Career.jsx
+++ app/_components/Career.jsx
@@ -6,7 +6,11 @@ export default function Career({ careerData }) {
   const [toggle, setToggle] = useState(false)
 
   return (
-    <div className='flex flex-col lg:items-center lg:pt-4 hover:scale-105'>
+    <div
+      className='flex flex-col lg:items-center lg:pt-4 hover:scale-105'
+      onMouseEnter={() => setToggle(true)}
+      onMouseLeave={() => setToggle(false)}
+    >
       <div className='bg-white p-6 w-[510px] lg:w-64 h-auto lg:mt-28 rounded-2xl mb-6 drop-shadow-md'>
         <div className='text-mygreen font-bold mb-4'>Career Summary</div>
 
@@ -19,7 +23,7 @@ export default function Career({ careerData }) {
                 <div>{item.period}</div>
               </div>
 
-              {toggle || item.employer !== 'Accounting d.o.o.' ? (
+              {toggle || item.employer === 'Experfy' ? (
                 <div className='ml-4 bg-white'>
                   <ul className='list-disc'>
                     {item.obligations.map((obligation, index) => (
@@ -29,27 +33,7 @@ export default function Career({ careerData }) {
                     ))}
                   </ul>
                 </div>
-              ) : (
-                <button
-                  aria-label='toggle details'
-                  onClick={() => setToggle(true)}
-                >
-                  <svg
-                    className='w-6 h-6'
-                    fill='none'
-                    stroke='currentColor'
-                    viewBox='0 0 24 24'
-                    xmlns='http://www.w3.org/2000/svg'
-                  >
-                    <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      strokeWidth='2'
-                      d='M19 9l-7 7-7-7'
-                    ></path>
-                  </svg>
-                </button>
-              )}
+              ) : null}
             </div>
           ))}
         </div>
