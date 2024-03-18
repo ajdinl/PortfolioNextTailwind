4bf4a7a943f26c3e252257c3bef7cd4121af0265
ajdinl <ajdinl@live.com>
2024-03-18 20:34:45 +0100

    update meta

--- app/head.jsx
+++ app/head.jsx
@@ -1,11 +1,16 @@
 export default function Head() {
   return (
     <>
-      <title>Ajdin Lojic&apos;s Portfolio</title>
+      <title>Ajdin Lojic's Portfolio</title>
       <meta
         name='description'
-        content='Ajdin Lojic Frontend Developer Portfolio React Next Ember'
+        content="Explore Ajdin Lojic's Web Developer Portfolio showcasing expertise in React, Node.js, Next.js, Express, and Ember.js."
       />
+      <meta
+        name='keywords'
+        content='Ajdin Lojic, Web Developer, Portfolio, React, Node.js, Next.js, Express, Ember.js, JavaScript, HTML, CSS'
+      />
+      <meta name='author' content='Ajdin Lojic' />
       <meta
         name='google-site-verification'
         content='I0bxza9FsmM34Q21YfY31hluNzPw6hJdo7t7rN1-um4'
