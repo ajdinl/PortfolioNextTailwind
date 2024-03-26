5e2ec859f14738dba80799b52cb5323e87b76724
ajdinl <ajdinl@live.com>
2024-03-26 08:22:57 +0100

    update metadata for google search console

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,27 @@
 import '@/styles/globals.css'
+import Head from 'next/head'
+
+export const metadata = {
+  title: "Ajdin Lojic's Portfolio",
+  description:
+    "Explore Ajdin Lojic's Web Developer Portfolio showcasing expertise in React, Node.js, Next.js, Express, and Ember.js.",
+  keywords: 'Ajdin Lojic, Web Developer, Portfolio, React, Node.js, Next.js, Express, Ember.js, JavaScript, HTML, CSS',
+  author: 'Ajdin Lojic',
+  googleSiteVerification: 'I0bxza9FsmM34Q21YfY31hluNzPw6hJdo7t7rN1-um4',
+  icon: '/favicon.ico',
+}
 
 export default function RootLayout({ children }) {
   return (
     <html suppressHydrationWarning>
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name='description' content={metadata.description} />
+        <meta name='keywords' content={metadata.keywords} />
+        <meta name='author' content={metadata.author} />
+        <meta name='google-site-verification' content={metadata.googleSiteVerification} />
+        <link rel='icon' href={metadata.icon} />
+      </Head>
       <body>{children}</body>
     </html>
   )
