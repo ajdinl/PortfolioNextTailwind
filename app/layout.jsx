dda1f478a9aeb8aa66b2e93568c634f46bb9fcaa
ajdinl <ajdinl@live.com>
2025-09-27 10:37:21 +0200

    Enhance layout metadata for portfolio, including updated title, description, keywords, and social links for improved SEO and user engagement.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,13 +1,35 @@
 import '@/styles/globals.css'
 
 export const metadata = {
-  title: "Ajdin Lojic's Portfolio",
+  title: 'Ajdin Lojic | Full Stack Web Developer (React, Node.js, Ruby on Rails)',
   description:
-    "Explore Ajdin Lojic's Web Developer Portfolio showcasing expertise in React, Node.js, Next.js, Express, and Ember.js.",
-  keywords: 'Ajdin Lojic, Web Developer, Portfolio, React, Node.js, Next.js, Express, Ember.js, JavaScript, HTML, CSS',
+    'Full Stack Developer skilled in React, Node.js, Ruby on Rails, Next.js & Nest.js. Building scalable, efficient, and user-friendly web applications with modern technologies.',
+  keywords:
+    'Ajdin Lojic, Web Developer, Full Stack Developer, React Developer, Node.js Developer, Ruby on Rails Developer, Next.js Developer, Nest.js Developer, JavaScript, TypeScript, Frontend, Backend, Portfolio',
   author: 'Ajdin Lojic',
+  creator: 'Ajdin Lojic',
+  publisher: 'Ajdin Lojic',
   googleSiteVerification: 'I0bxza9FsmM34Q21YfY31hluNzPw6hJdo7t7rN1-um4',
-  icon: '/favicon.ico',
+  icons: {
+    icon: '/favicon.ico',
+  },
+  openGraph: {
+    title: 'Ajdin Lojic | Full Stack Web Developer',
+    description:
+      'Explore Ajdin Lojic’s portfolio showcasing expertise in React, Node.js, Ruby on Rails, Next.js, Express & Nest.js.',
+    url: 'https://ajdinlojic.vercel.app/',
+    siteName: 'Ajdin Lojic Portfolio',
+    locale: 'en_US',
+    type: 'website',
+  },
+  social: {
+    linkedIn: {
+      url: 'https://www.linkedin.com/in/ajdin-lojic/',
+      title: 'Ajdin Lojic | Full Stack Web Developer',
+      description:
+        'Check out my professional portfolio highlighting expertise in React, Node.js, Ruby on Rails, Next.js, Express & Nest.js.',
+    },
+  },
 }
 
 export default function RootLayout({ children }) {
